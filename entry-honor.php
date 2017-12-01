6388d190a4fef94a8b60bc84890632526df4b17b
Grace <grace_girly_melody@yahoo.com>
2017-12-01 13:31:40 +0700

    first commit whew

---
+++ entry-honor.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Event Organizer Management System</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="styles.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+  <script>
+  // Bootstrap tooltip, https://www.w3schools.com/bootstrap/bootstrap_tooltip.asp
+  $(document).ready(function(){
+      $('.btn-watch').tooltip();
+  });
+  </script>
+</head>
+<body>
+
+<div class="container">
+<h1>Event Organizer Management System</h1>
+<h3>Bima, Evans, Grace, Yuan - 3115100??, 311710008, 311710010,</h3>
