230894d3c64c4bf771514b578c6348802f2d69d6
Evans Jahja <evans.jahja@yahoo.com>
2017-12-01 23:12:55 +0700

    WIP: Entry honor

--- entry-honor.php
+++ entry-honor.php
@@ -1,4 +1,143 @@
+<?php require('php_header.php') ?>
+
+
+<?php
+
+  if (isset($_POST['setActive'])) {
+    // Set active
+    $query = "UPDATE event SET aktif=? WHERE id=?";
+    $stmt = $db->prepare($query);
+    $stmt->bind_param("ii", $_POST['setActive'], $_POST['id']);
+    $stmt->execute();
+    echo $query;
+    echo $_POST['setActive'];
+    echo $_POST['id'];
+    die();
+  }
+  
+  if (isset($_POST['submit'])) {
+    // Tambah event
+
+    $query = "INSERT INTO event (nama, id_wilayah, hari, waktu_mulai, waktu_selesai, aktif) VALUES (?, ?, ?, ?, ?, ?)";
+
+    $stmt = $db->prepare($query);
+    $true_bool = true;
+    $stmt->bind_param("sisssi", $_POST['nama_event'], $_POST['id_wilayah'], $_POST['hari'], $_POST['jam_mulai'], $_POST['jam_selesai'], $true_bool);
+    $stmt->execute() or die($db->error);
+  }
+
+  function populateTable() {
+    require('db.php');
+    
+    if (isset($_GET['id_wilayah'])) {
+      $query = "SELECT 
+                  honor.id,
+                  personal.nama
+                  FROM honor
+                  INNER JOIN personal
+                  ON honor.id_personal=personal.nip
+                  INNER JOIN event
+                  ON id_event=event.id
+                  WHERE id_wilayah=?";
+      $stmt = $db->prepare($query) or die($db->error);
+      $stmt->bind_param('i',$_GET['id_wilayah']);
+    } else {
+      $query = "SELECT honor.id, personal.nama FROM honor INNER JOIN personal ON honor.id_personal=personal.nip";
+      $stmt = $db->prepare($query) or die($db->error);
+    }
+    
+    $stmt->execute();
+    $stmt->bind_result($id, $nama_personal);
+
+    while ($stmt->fetch()) {
+    ?>
+          <tr>
+           <td><?php echo $id ?></td>
+           <td><?php echo $nama_personal ?></td>
+           <td>john@example.com</td>
+           <td><button type="button" class="btn btn-danger">Hapus</button></td>
+         </tr>
+    <?php
+    }
+  }
+  
+  function populateHari() {
+    ?>
+    <option value="Senin">Senin</option>
+    <option value="Selasa">Selasa</option>
+    <option value="Rabu">Rabu</option>
+    <option value="Kamis">Kamis</option>
+    <option value="Jumat">Jumat</option>
+    <option value="Sabtu">Sabtu</option>
+    <option value="Minggu">Minggu</option>
+    <?php
+  }
+  
+  function populateOptionPerson() {
+    
+    require('db.php');
+    $query = "SELECT nip, nama FROM personal WHERE aktif = 1";
+    $stmt = $db->prepare($query);
+    $stmt->execute();
+    $stmt->bind_result($id, $nama);
+
+    while ($stmt->fetch()) {
+      ?>
+      
+      <option value="<?php echo $id ?>"><?php echo $nama ?></option>
+      
+      <?php
+    }
+  }
+  
+  function populateOptionPosisi() {
+    
+    require('db.php');
+    $query = "SELECT id, nama FROM posisi WHERE aktif = 1";
+    $stmt = $db->prepare($query);
+    $stmt->execute();
+    $stmt->bind_result($id, $nama);
+
+    while ($stmt->fetch()) {
+      ?>
+      <option value="<?php echo $id ?>"><?php echo $nama ?></option>
+      
+      <?php
+    }
+  }
+  
+  function populateOptionWilayah() {
+    
+    require('db.php');
+    $query = "SELECT id, nama FROM wilayah WHERE aktif = 1";
+    $stmt = $db->prepare($query);
+    $stmt->execute();
+    $stmt->bind_result($id, $nama_wilayah);
+
+    ?>
+    <option disabled selected>---</option>
+    <?php
+    while ($stmt->fetch()) {
+      ?>
+      
+      <option value="<?php echo $id ?>"><?php echo $nama_wilayah ?></option>
+      
+      <?php
+    }
+  }
+?>
+
 <?php require('header.php') ?>
+
+
+<script>
+  $(document).ready(function() {
+    $('#select_wilayah').change(function() {
+      window.location = "entry-honor.php?id_wilayah="+$(this).prop("value");
+    })
+  })
+</script>
+
               <li>
                 <a href="#">Data Master</a>
                 <ul class="nav padder">
@@ -27,16 +166,13 @@
         <div class="row">
           <div class="form-group col-xs-4">
             <label for="sel1">Wilayah:</label>
-            <select class="form-control" id="wilayah">
-              <option>Malang</option>
-              <option>Surabaya</option>
-              <option>Pasuruan</option>
-              <option>Sidoarjo</option>
+            <select class="form-control" id="select_wilayah">
+              <?php populateOptionWilayah() ?>
             </select>
           </div>
           <div class="form-group col-xs-4">
             <label for="sel1">Event:</label>
-            <select class="form-control" id="wilayah">
+            <select class="form-control" id="event">
               <option>Colony Cafe</option>
               <option>Black Castle</option>
               <option>Java Dancer</option>
@@ -59,12 +195,7 @@
          </tr>
        </thead>
        <tbody>
-         <tr>
-           <td>John</td>
-           <td>Doe</td>
-           <td>john@example.com</td>
-           <td><button type="button" class="btn btn-danger">Hapus</button></td>
-         </tr>
+       <?php populateTable() ?>
        </tbody>
      </table>
 
@@ -74,11 +205,11 @@
           <div class="form-group">
             <label for="nama">Nama:</label>
             <select class="form-control" id="nama">
-              <option>John Doe</option>
+              <?php populateOptionPerson(); ?>
             </select>
               <label for="nama">Posisi:</label>
               <select class="form-control" id="nama">
-                <option>1</option>
+                <?php populateOptionPosisi() ?>
               </select>
             </div>
           </div>
