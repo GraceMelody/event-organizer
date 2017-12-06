16ba660fd76ef1a706814f88a1695c8bb2c0a597
Evans Jahja <evans.jahja@yahoo.com>
2017-12-06 17:48:16 +0700

    Detail honor table

--- detail.php
+++ detail.php
@@ -10,6 +10,35 @@
     $_GET['id_user'] = getNIP();
   }
 ?>
+<?php 
+  function populateTable() {
+    require('db.php');
+    $query = "SELECT honor.id, posisi.nama, wilayah.nama, event.nama, DATE(honor.tanggal_event), FORMAT(honor.gaji, 2, 'de_DE') FROM honor
+    INNER JOIN posisi ON honor.id_posisi = posisi.id
+    INNER JOIN event ON honor.id_event = event.id
+    INNER JOIN wilayah ON event.id_wilayah = wilayah.id
+    WHERE DATE(tanggal_event) BETWEEN DATE(?) AND DATE(?) AND  id_personal=?";
+    $stmt = $db->prepare($query) or show_error_dialog($db->error);
+    $stmt->bind_param('ssi', $_GET['begin_date'], $_GET['end_date'], $_GET['id_user']);
+    $stmt->execute();
+    $stmt->bind_result($id, $posisi, $wilayah, $event, $date, $gaji);
+
+    while ($stmt->fetch()) {
+    ?>
+          <tr>
+            <td><?php echo $id ?></td>
+            <td><?php echo $posisi ?></td>
+            <td><?php echo $wilayah ?></td>
+            <td><?php echo $event ?></td>
+            <td><?php echo $date ?></td>
+            <td class="text-right"><?php echo $gaji ?></td>
+            <td><a href="detail.php?id_user=<?php echo $id ?>"class="btn btn-default">Detail</a></td>
+          </tr>
+    <?php
+    }
+  }
+  
+?>
 <?php
   $query = "SELECT personal.nama, posisi.nama FROM personal INNER JOIN posisi ON personal.id_posisi=posisi.id WHERE personal.nip=?";
   $stmt = $db->prepare($query) or show_error_dialog($db->error);
@@ -19,6 +48,28 @@
   $stmt->fetch();
 ?>
 <?php require('header.php') ?>
+<script>
+  $(document).ready(function(){
+    //https://css-tricks.com/snippets/jquery/get-query-params-object/
+    jQuery.extend({
+      getQueryParameters : function(str) {
+        return (str || document.location.search).replace(/(^\?)/,'').split("&").map(function(n){return n = n.split("="),this[n[0]] = n[1],this}.bind({}))[0];
+      }
+    });
+    
+    $('#begin_date').change(function() {
+      const qs = $.getQueryParameters();
+      qs.begin_date = $(this).prop("value")
+      window.location = "detail.php?" + $.param(qs);
+    })
+    
+    $('#end_date').change(function() {
+      const qs = $.getQueryParameters();
+      qs.end_date = $(this).prop("value")
+      window.location = "detail.php?" + $.param(qs);
+    })
+  })
+</script>
               <?php if(canEditMaster()) { ?>
               <li>
                 <a href="#">Data Master</a>
@@ -54,8 +105,8 @@
         <div class="row">
           <div class="form-group">
             <label for="sel1" class="col-lg-2 col-sm-12">Periode:</label>
-            <input type="date" class="form-control pad15 col-sm-12 col-lg-3 periode" id="sel1">
-            <input type="date" class="form-control pad15 col-sm-12 col-lg-3 periode" id="sel2">
+            <input type="date" class="form-control pad15 col-sm-12 col-lg-3 periode" id="begin_date" <?php echo isset($_GET['begin_date']) ? 'value='.$_GET['begin_date'] : '' ?>>
+            <input type="date" class="form-control pad15 col-sm-12 col-lg-3 periode" id="end_date">
         </div>
 
           <div class="form-group col-xs-9">
@@ -78,12 +129,7 @@
          </tr>
        </thead>
        <tbody>
-         <tr>
-           <td>John</td>
-           <td>Doe</td>
-           <td>john@example.com</td>
-           <td><a href="detail.php"class="btn btn-default">Detail</a></td>
-         </tr>
+         <?php populateTable() ?>
        </tbody>
      </table>
      </div>
