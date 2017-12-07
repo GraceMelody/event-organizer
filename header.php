39a8f9dd49835512982d819d35bc539088323f87
Grace <grace_girly_melody@yahoo.com>
2017-12-07 20:56:52 +0700

    modified detail.php a bit

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
     <h1>Event Organizer Management System</h1>
     <h3>Bima, Evans, Grace, Yuan - 311510005, 311710008, 311710010, 311510025</h3></span>
   </div>
-  <a href="logout.php"><span class="btn  col-lg-offset-11 col-md-offset-10 col-sm-offset-7 col-xs-offset-0 glyphicon glyphicon-log-out"></span> Log Out</a>
+
   <div class="col-xs-12 col-sm-3">
     <nav class="navbar navbar-default">
       <div class="container-fluid">
